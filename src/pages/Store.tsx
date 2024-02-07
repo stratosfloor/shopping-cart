4c3d6b07ba7736b344f9faeca157913d39e12c88
Emil Noren <stratosfloor@hotmail.com>
2024-02-07 14:49:33 +0100

    Change typo

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -6,7 +6,7 @@ export function Store() {
 	return (
 		<>
 			<h1>Store</h1>
-			<Row md={2} xs={1} lg={1} className="g-3">
+			<Row xs={1} md={2} lg={3} className="g-3">
 				{storeItems.map((item) => (
 					<Col key={item.id}>
 						<StoreItem {...item} />
