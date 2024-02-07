a9aebd495f0a63b5093ea9b0b0707610e39efbe1
Emil Noren <stratosfloor@hotmail.com>
2024-02-07 11:27:31 +0100

    Implemente store item

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,64 @@
+import { Button, Card } from 'react-bootstrap';
+import { formatCurrency } from '../utilites/formatCurrency';
+
+type StoreItemProps = {
+	id: number;
+	name: string;
+	price: number;
+	imgUrl: string;
+};
+
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+	const quantity = 0;
+
+	return (
+		<Card className="h-100">
+			<Card.Img
+				variant="top"
+				src={imgUrl}
+				height="200px"
+				style={{ objectFit: 'cover' }}
+			/>
+			<Card.Body className="d-flex flex-column">
+				<Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+					<span className="fs-2">{name}</span>
+					<span className="ms-2 text-muted">{formatCurrency(price)}</span>
+				</Card.Title>
+				<div className="mt-auto d-flex justify-content-center">
+					{quantity === 0 ? (
+						<Button className="w-50">+ Add to cart</Button>
+					) : (
+						<div
+							className="d-flex align-items-center flex-column"
+							style={{ gap: '.5rem' }}
+						>
+							<div
+								className="d-flex align-items center justify-content-center"
+								style={{ gap: '.5rem' }}
+							>
+								<Button
+									className="d-flex justify-content-center align-items-center"
+									style={{ width: '2rem', height: '2rem' }}
+								>
+									-
+								</Button>
+								<div>
+									<span className="fs-3">{quantity}</span> in cart
+								</div>
+								<Button
+									className="d-flex justify-content-center align-items-center"
+									style={{ width: '2rem', height: '2rem' }}
+								>
+									+
+								</Button>
+							</div>
+							<Button variant="outline-danger" size="sm">
+								<span>Remove</span>
+							</Button>
+						</div>
+					)}
+				</div>
+			</Card.Body>
+		</Card>
+	);
+}
